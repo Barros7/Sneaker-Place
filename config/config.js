f91f2bd6287e67db02c029b7c35d64eb83c9688c
José Bongo <josebarros412@gmail.com>
2022-09-17 05:59:33 +0100

    ha

--- config/config.js
+++ config/config.js
@@ -5,10 +5,10 @@ dotenv.config();
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME;
 const SERVER_PORT = process.env.SERVER_PORT;
 
-const MYSQL_HOST = process.env.MYSQL_HOST;
-const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
-const MYSQL_USER = process.env.MYSQL_USER;
-const MYSQL_PASS = process.env.MYSQL_PASS;
+const MYSQL_HOST = process.env.MYSQL_HOST || 'eu-cdbr-west-03.cleardb.net';
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'heroku_ddae54556865055';
+const MYSQL_USER = process.env.MYSQL_USER || 'b552ee61fdca55';
+const MYSQL_PASS = process.env.MYSQL_PASS || 'f5ec5d02';
 
 const config = {
   server: {
