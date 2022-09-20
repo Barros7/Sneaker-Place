790133e02365a3296e98db1c9b00d4cd67c20f44
José Bongo <josebarros412@gmail.com>
2022-09-20 12:52:37 +0100

    Add new collumn on data base

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME;
 const SERVER_PORT = process.env.SERVER_PORT;
 
-const MYSQL_HOST = process.env.MYSQL_HOST || 'eu-cdlbr-west-03.cleardb.net';
+const MYSQL_HOST = process.env.MYSQL_HOST || 'eu-cdbr-west-03.cleardb.net';
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'heroku_ddae54556865055';
 const MYSQL_USER = process.env.MYSQL_USER || 'b552ee61fdca55';
 const MYSQL_PASS = process.env.MYSQL_PASS || 'f5ec5d02';
