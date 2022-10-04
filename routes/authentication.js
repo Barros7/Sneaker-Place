b5a4ee2ec73b394823edd43aa6ebfaae39084bb6
José Bongo <josebarros412@gmail.com>
2022-10-04 23:03:09 +0100

    ...

--- routes/authentication.js
+++ routes/authentication.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/login",login);
 router.post("/signIn", createUser);
-router.put("/updateUser", updateUser);
+router.post("/updateUser", updateUser);
 
 export default router;
